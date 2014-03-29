91de51005fd5252659199e439826dcf722f6516c
meggers <meggers@wisc.edu>
2014-03-29 18:51:24 -0500

    added input/output bus widths

--- ScalarRegFile.v
+++ ScalarRegFile.v
@@ -1,7 +1,10 @@
 module ScalarRegFile (rd_addr_1, rd_addr_2, wr_dst, wr_data, wr_en, data_1, data_2);
 
-input rd_addr_1, rd_addr_2, wr_dst, wr_data, wr_en;
-output data_1, data_2;
+input wr_en;
+input [3:0] rd_addr_1, rd_addr_2, wr_dst;
+input [15:0] wr_data;
+
+output [15:0] data_1, data_2;
 
 
 
