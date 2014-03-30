ddc0e5f34bedeff09225525a93ce8ae6db9eb7c1
meggers <meggers@wisc.edu>
2014-03-29 19:20:57 -0500

    finished first draft scalar reg file

--- ScalarRegFile.v
+++ ScalarRegFile.v
@@ -1,11 +1,22 @@
-module ScalarRegFile (rd_addr_1, rd_addr_2, wr_dst, wr_data, wr_en, data_1, data_2);
+module ScalarRegFile(rd_addr_1, rd_addr_2, wr_dst, wr_data, wr_en, data_1, data_2);
 
 input wr_en;
 input [3:0] rd_addr_1, rd_addr_2, wr_dst;
 input [15:0] wr_data;
 
-output [15:0] data_1, data_2;
+wire [15:0] test;
 
+output reg [15:0] data_1, data_2;
 
+reg [7:0] regTable [15:0];
+
+always @(*) begin
+  data_1 = regTable[rd_addr_1];
+  data_2 = regTable[rd_addr_2];
+  
+  if (wr_en) begin
+    regTable[wr_dst] = wr_data;
+  end
+end
 
 endmodule
