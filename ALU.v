b244e82052f9ff6f989187ca6406f98bf5da1f8f
meggers <meggers@wisc.edu>
2014-03-30 15:25:37 -0500

    rewrote scalar reg file and continued with ALU

--- ALU.v
+++ ALU.v
@@ -8,12 +8,24 @@ module VADD (clk, op_1, op_2, sum);
   input  [255:0] op_1, op_2;
   output [255:0] sum;
   
-  reg [4:0] dimension;
+  reg [5:0] dimension;
   
-  localparam maxDimensions = 15;
+  localparam maxDimensions = 16,
+             dimensionSize = 16;
+  
+  reg [15:0] vector_1 [15:0];
+  reg [15:0] vector_2 [15:0];
+  
+  reg [15:0] vector_sum [15:0];
   
   always @(posedge clk) begin
-    for (dimension = 0; dimension <= maxDimensions; dimension = dimension + 1) begin
+    sum <= vector_sum;
+  end
+  
+  always @(op_1, op_2) begin
+    for (dimension = 0; dimension < maxDimensions; dimension = dimension + 1) begin
+      vector_1[dimension] <= op_1[dimensionSize * dimension +: dimensionSize];
+      vector_2[dimension] <= op_2[dimensionSize * dimension +: dimensionSize];
       
     end
   end
--- ScalarRegFile.v
+++ ScalarRegFile.v
@@ -1,6 +1,6 @@
-module ScalarRegFile(rd_addr_1, rd_addr_2, wr_dst, wr_data, wr_en, data_1, data_2);
+module ScalarRegFile(clk, rd_addr_1, rd_addr_2, wr_dst, wr_data, wr_en, data_1, data_2);
 
-input wr_en;
+input clk, wr_en;
 input [3:0] rd_addr_1, rd_addr_2, wr_dst;
 input [15:0] wr_data;
 
@@ -8,9 +8,9 @@ output reg [15:0] data_1, data_2;
 
 reg [7:0] regTable [15:0];
 
-always @(*) begin
-  data_1 = regTable[rd_addr_1];
-  data_2 = regTable[rd_addr_2];
+always @(posedge clk) begin
+  data_1 <= regTable[rd_addr_1];
+  data_2 <= regTable[rd_addr_2];
   
   if (wr_en) begin
     regTable[wr_dst] = wr_data;
