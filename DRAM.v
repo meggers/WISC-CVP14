48ef46daefcad6ced0a6a15a16227a518db69ea0
Aarskin <mwiemer@outlook.com>
2014-03-30 16:14:22 -0500

    Untested DRAM written

--- DRAM.v
+++ DRAM.v
@@ -1,14 +1,24 @@
-module DRAM(output[15:0] DataOut, input [15:0] Addr, input Clk1, input Clk2, input [15:0] DataIn, input RD, input WR);
+module DRAM(output[15:0] DataOut, input [15:0] Addr, input Clk1,
+  input Clk2, input [15:0] DataIn, input RD, input WR);
   
-  [15:0] mem [15:0]; // The memory structure
+  reg latchedRD, latchedWR;
+  reg [15:0] latchedAddr, latchedDataOut, latchedDataIn;
+  reg [15:0] mem [15:0]; // The memory structure
+  
+  assign DataOut = latchedDataOut;
   
   always @(posedge Clk1) begin
-    if(RD)
-      DataOut = mem[Addr]
+    latchedAddr <= Addr;
+    
+    if(latchedRD)
+      latchedDataOut <= mem[latchedAddr];
+    if(latchedWR)
+      mem[latchedAddr] <= latchedDataIn;
   end
   
   always @(posedge Clk2) begin
-    if(WR)
-      
+    latchedRD <= RD;
+    latchedWR <= WR;
+    latchedDataIn <= DataIn;
   end
 endmodule
--- DRAM_tb.v
+++ DRAM_tb.v
@@ -1,6 +1,18 @@
 module DRAM_tb ();
-  wire Clk1, Clk2;
+  wire Clk1, Clk2, RD, WR;
+  wire [15:0] DataOut, DataIn, Addr; 
   
   clock_source clocks(.Clk1(Clk1), .Clk2(Clk2));
+  
+  DRAM memory(.DataOut(DataOut), 
+              .DataIn(DataOut), 
+              .Addr(Addr), 
+              .Clk1(Clk1), 
+              .Clk2(Clk2), 
+              .RD(RD), .WR(WR));
+  
+  initial begin
+    
+  end
 
 endmodule
