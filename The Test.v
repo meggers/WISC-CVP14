f3ca4c79dc48002909f09302f283c9fbe910ecf6
Aarskin <mwiemer@outlook.com>
2014-04-03 13:04:51 -0500

    Wrote the test

--- The Test.v
+++ The Test.v
@@ -1,9 +1,34 @@
 module the_test();
-  wire Clk1, Clk2;
+  reg rst;
+
+  wire Clk1, Clk2, V;
+  wire [15:0] dataTo, dataFrom, addr; // wrt to DRAM
   
   clock_source clocks(.Clk1(Clk1), .Clk2(Clk2));
   
-  CVP14 processor();
+  CVP14 processor(.Clk1(Clk1), 
+                  .Clk2(Clk2), 
+                  .Reset(rst), 
+                  .DataIn(dataFrom),
+                  
+                  .Addr(addr),
+                  .RD(RD),
+                  .WR(WR),
+                  .V(V),
+                  .dataOut(dataTo));
   
-  staticram DRAM();
+  staticram DRAM(.clk1(Clk1), 
+                 .clk2(Clk2),
+                 .Addr(addr),
+                 .DataIn(dataTo), 
+                 .RD(RD), 
+                 .WR(WR),
+                 
+                 .DataOut(dataFrom));
+                 
+  initial begin
+    rst = 1'b1;
+    #2;
+    rst = 1'b0;
+  end
 endmodule
